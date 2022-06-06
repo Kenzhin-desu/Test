fb4257f2c3d681db8f96b354dd9a52a476009800
kenzhin <kenzhin.ancheta07@gmail.com>
2022-06-06 19:14:11 +0800

    init

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-document.getElementById("count-el").innerText
+// document.getElementById("count-el").innerText
+
+//let firstBatch = 5
+//let secondBatch = 7
+
+//let count = firstBatch * secondBatch
+ 
+
+
+//convert dog age 
+/*
+let myAge = 15
+
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+*/
+// create bonus points
+/*
+let BonusPoints = 50
+console.log(BonusPoints)
+
+BonusPoints = BonusPoints + 50
+console.log(BonusPoints)
+
+BonusPoints = BonusPoints - 25
+console.log(BonusPoints)
+
+BonusPoints = BonusPoints + 70
+
+
+console.log(BonusPoints)
+*/
+//function
+/*
+function increment()   {
+    console.log ("the button was clicked")
+}
+*/
+/*
+function theMeaningOfLife() {
+    console.log(42)
+}
+theMeaningOfLife()
+*/
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function Laptime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+    // or console.log(lap1 + lap2 + lap3)
+}
+Laptime()
