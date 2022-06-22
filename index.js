ce1354d82803567a5261da8828d2d8b8b0a41f7a
kenzhin <kenzhin.ancheta07@gmail.com>
2022-06-22 18:30:42 +0800

    updated files

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 
 let myLeads = []
-
-
+let oldLeads = []
+const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
+ if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+ }
+
+saveBtn.addEventListener("click", function(){
+    console.log(tabs[0].url)
+})
+const tabs = [
+    {url: "https://www.facebook.com"}
+]
+
+
+
+ function render(leads){
+    let listItem = ""
+    for (let i = 0; i< leads.length; i++) {
+    
+       // listItem += <li>" + "<a href='" + myLeads[i] + "' target='_black'>" + myLeads[i] + "</a>" + "</li>"
+       listItem += `
+        <li>
+            <a href='${leads[i]}' target='_black'>
+                ${leads[i]}
+            </a>
+        </li>`
+        /*const li = document.createElement("li")
+        li.textContent = myLeads[i]
+        ulEl.append(li)*/
+        
+    }
+    ulEl.innerHTML =  listItem
+    }
 //localStorage.setItem("myleads", "www.awesomesauce.com")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 
     console.log( localStorage.getItem("myLeads") )
 })
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-function renderLeads(){
-let listItem = ""
-for (let i = 0; i< myLeads.length; i++) {
-
-   // listItem += <li>" + "<a href='" + myLeads[i] + "' target='_black'>" + myLeads[i] + "</a>" + "</li>"
-   listItem += `
-    <li>
-        <a href='${myLeads[i]}' target='_black'>
-            ${myLeads[i]}
-        </a>
-    </li>`
-   console.log(listItem)
-    /*const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)*/
-    
-}
-ulEl.innerHTML =  listItem
-}
--- test.js
+++ test.js
@@ -1,12 +1,6 @@
 
+function getFirst(arr){
+    return arr[0]
+}
 
-const sender = "Kenzhin"
-const recipients = "James"
-
-const email = `
-    Hey ${recipients}!
-    How is it going?
-    Cheers ${sender}
-`
-
-console.log(email)
+console.log(getFirst(["get", "first"]))
